875146f65c8b626c98f18ae91656911d9817720f
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2023-01-29 20:19:18 +0100

    Define the relations and DBSet for all the tables

--- eTickets/eTickets/Data/AppDBContext.cs
+++ eTickets/eTickets/Data/AppDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using eTickets.Models;
 
 namespace eTickets.Data
 {
@@ -8,5 +9,27 @@ namespace eTickets.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m=>m.Movie).WithMany(am=>am.Actor_Movies).HasForeignKey(m=>m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(a => a.Actor).WithMany(am => am.Actor_Movies).HasForeignKey(a => a.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinmeas { get; set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+
+        
+
     }
 }
