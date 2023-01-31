ee627388be7d24677e48d8c49d014f7be31c25cb
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2023-01-31 20:03:45 +0100

    Added Naigation Added to include cinema name and order by clause Added launchurl Display the movie in card format

--- eTickets/eTickets/Controllers/MoviesController.cs
+++ eTickets/eTickets/Controllers/MoviesController.cs
@@ -16,7 +16,7 @@ namespace eTickets.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
+            var allMovies = await _context.Movies.Include(m=>m.Cinema).OrderBy(n=>n.Name).ToListAsync();
             return View(allMovies);
         }
     }
